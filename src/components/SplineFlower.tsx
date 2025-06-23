792be676bd5562291ccc160b476db830713fe8f4
aditya das <55331202+chainsawmaiden@users.noreply.github.com>
2025-06-23 00:06:53 -0400

    Update SplineFlower.tsx

--- src/components/SplineFlower.tsx
+++ src/components/SplineFlower.tsx
@@ -3,7 +3,7 @@ import Spline from '@splinetool/react-spline/next';
 export default function SplineFlower() {
   return (
     <div className="spline-flower-container">
-      <Spline
+      <Spline className='spline-flower'
         scene="https://prod.spline.design/yVGRCwkstbPpZofU/scene.splinecode" 
       />
     </div>
