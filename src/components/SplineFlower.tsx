0f52eb5397d6851d9a3c5b9342e7f2af874596ef
aditya das <55331202+chainsawmaiden@users.noreply.github.com>
2025-06-22 23:47:16 -0400

    blub

--- src/components/SplineFlower.tsx
+++ src/components/SplineFlower.tsx
@@ -1,50 +1,11 @@
-'use client';
+import Spline from '@splinetool/react-spline/next';
 
-import { useEffect, useRef } from 'react';
-
-export default function SplineFlowerAlt() {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-
-  useEffect(() => {
-    console.log('SplineFlowerAlt component mounted');
-    
-    const loadSpline = async () => {
-      try {
-        const { Application } = await import('@splinetool/runtime');
-        
-        if (canvasRef.current) {
-          const app = new Application(canvasRef.current);
-          await app.load('https://prod.spline.design/E3BeEaHP4XLHK-Oz/scene.splinecode');
-          console.log('Spline loaded with runtime');
-        }
-      } catch (error) {
-        console.error('Error loading Spline:', error);
-      }
-    };
-
-    loadSpline();
-  }, []);
-
+export default function SplineFlower() {
   return (
-    <div className="flower-container">
-      <div 
-        className="spline-flower-container"
-        style={{
-          width: '800px',
-          height: '600px',
-          aspectRatio: '4/3',
-          position: 'relative'
-        }}
-      >
-        <canvas 
-          ref={canvasRef}
-          style={{
-            width: '100%',
-            height: '100%',
-            display: 'block'
-          }}
-        />
-      </div>
+    <div >
+      <Spline
+        scene="https://prod.spline.design/yVGRCwkstbPpZofU/scene.splinecode" 
+      />
     </div>
   );
 }
